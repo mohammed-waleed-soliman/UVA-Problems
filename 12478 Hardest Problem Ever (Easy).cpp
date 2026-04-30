ff8d36e86999dae02f1bf33f0cd31314b823b679
Mohammed Waleed <136664646+mohammed-waleed-soliman@users.noreply.github.com>
2026-04-30 16:22:43 +0300

    Create 12478 Hardest Problem Ever (Easy).cpp

---
+++ 12478 Hardest Problem Ever (Easy).cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution(){
+  cout << "KABIR" << endl;
+}
+
+int main(){
+  int ntimes = 1;
+  while (ntimes--)
+  {
+    solution();
+  }
+}
