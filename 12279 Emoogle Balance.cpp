1125a3a4a5354d3a875c6eafdc02fb70fcd7130a
Mohammed Waleed <136664646+mohammed-waleed-soliman@users.noreply.github.com>
2026-04-30 17:28:37 +0300

    Update 12279 Emoogle Balance.cpp

--- 12279 Emoogle Balance.cpp
+++ 12279 Emoogle Balance.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
 
+int main(){
+  int ntimes = 1;
+  ll k, val, i = 1;
+  while (scanf("%lld", &k))
+  {
+    if(k==0) return 0;
+    ll a = 0, b = 0;
+    while (k--)
+    {
+      cin >> val;
+      if(val) a++;
+      else b++;
+    }
+    cout << "Case " << i++ << ": ";
+    cout << a-b << endl;
+  }
+}
