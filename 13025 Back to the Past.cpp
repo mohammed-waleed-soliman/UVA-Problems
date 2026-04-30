311d29fdf221a7ab6a0ae426a5b83589561b1048
Mohammed Waleed <136664646+mohammed-waleed-soliman@users.noreply.github.com>
2026-04-30 16:17:55 +0300

    Create 13025 Back to the Past.cpp

---
+++ 13025 Back to the Past.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution(){
+  cout << "May 29, 2013 Wednesday" << el;
+}
+
+int main(){
+  int ntimes = 1;
+  while (ntimes--)
+  {
+    solution();
+  }
+}
